0fa131eccde4a5efac91baedd50333566444af1f
Bella <bella.kallen2014@gmail.com>
2024-01-28 19:53:22 +0300

    test failed

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,5 +1,23 @@
 package ru.netology.rest;
 
-public class PostmanEchoTest {
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData(){
+        var text = "Hi";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("text")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(text));
+        ;
+    }
 }
